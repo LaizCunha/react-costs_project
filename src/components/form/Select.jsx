5231d34efb3b8044b4b45e332b23e48fdcd1303d
Laiz <laizsc.89@gmail.com>
2024-04-16 15:41:10 -0300

    Cria a API para cadastrar os projetos:
    *Cria scritp para rodar o backend;
    *Cria arquivo db.json para guardar as informações inseridas no site;
    *Modifica o component Select que busca da api as categorias de orçamentos para serem selecionadas no site ao cadastrar um novo projeto;
    *Modifica o component ProjectForm utilizando dos hooks useState e useEffetc para fazer requisição na api para preenchimento da categoria de orçamento.

--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -6,6 +6,9 @@ function Select({text, name, otpions, handleOnChange, value}) {
       <label htmlFor={name}>{text}:</label>
       <select name={name} id={name}>
         <option>Selecione uma opção</option>
+        {otpions.map((option) => (
+          <option value={option.id} key={option.id}>{option.name}</option>
+        ))}
       </select>
     </div>
   ) 
--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 import "./ProjectForm.css"
 
 function ProjectForm({btnText}) {
+
+  const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+
+      fetch("http://localhost:5000/categories")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data)
+      })
+      .catch((err) => console.log(err));
+
+    }, [])
+
   return (
     <form className="form">
       <Input 
@@ -21,6 +37,7 @@ function ProjectForm({btnText}) {
       <Select 
         name="category_id" 
         text="Selecione a categoria"
+        otpions={categories}
       />
       <SubmitButton 
         text={btnText}
