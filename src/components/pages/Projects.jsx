a53a5382d6403a55e35f08cf66b1ce726bc3070e
Laiz <laizsc.89@gmail.com>
2024-04-24 09:54:43 -0300

    Edita a page Projects:
    *Insere componente de LinkButton de criar projeto;
    *Ajusta estilo da página.

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,5 +1,8 @@
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
+import Container from '../layout/Container';
+import LinkButton from '../layout/LinkButton';
+import './Projects.css';
 
 function Projects() {
 
@@ -10,12 +13,22 @@ function Projects() {
   }
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
-      {message && <Message msg={message} type='success'/>}
-    </div>
+    <>
+      <div className="project_container">
+        <div className="title_container">
+          <h1 >Meus Projetos</h1>
+          <LinkButton to="/newproject" text="Criar Projeto" />
+        </div>
+        <div>  
+          {message && <Message msg={message} type='success'/>}
+          <Container customClass="start">
+            <p>Projetos</p>
+          </Container>
+        </div>
+      </div>
+
+    </>
   )
-    
 }
 
 export default Projects;
