f1bbac1ed3e8cfd24ee9d2410b15be17ed460ff4
Laiz <laizsc.89@gmail.com>
2024-04-24 16:50:51 -0300

    Cria o component Loading.

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -5,6 +5,7 @@ import Message from "../layout/Message";
 import Container from '../layout/Container';
 import LinkButton from '../layout/LinkButton';
 import ProjectCard from "../project/ProjectCard";
+import Loading from "../layout/Loading";
 
 import './Projects.css';
 
@@ -12,6 +13,7 @@ import './Projects.css';
 function Projects() {
 
   const [projects, setProjects] = useState([]);
+  const [removeLoading, setRemoveLoading] = useState(false);
 
   const location = useLocation();
   let message = '';
@@ -20,17 +22,24 @@ function Projects() {
   }
   
   useEffect(() => {
-    fetch("http://localhost:5000/projects", {
+
+    setTimeout(() => {
+
+      fetch("http://localhost:5000/projects", {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
-    })
+      })
       .then(resp => resp.json())
       .then((data) => {
         console.log(data)
-        setProjects(data)})
+        setProjects(data)
+        setRemoveLoading(true)
+      })
       .catch((err) => console.log(err))
+
+    }, 500)
   },[]);
 
   return (
@@ -52,6 +61,10 @@ function Projects() {
                 key={project.id}
               />
           ))}
+          {!removeLoading && <Loading/>}
+          {removeLoading && projects.length === 0 && (
+            <p>Não há projetos cadastrados!</p>
+          )}
         </Container>
       </div>
     </div>
